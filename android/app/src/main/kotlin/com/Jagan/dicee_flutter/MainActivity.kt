81e6517d2f534111ff0f639c63d8fc04ebd7fb5c
Jagan <sjagansapavat861@gmail.com>
2022-07-12 11:36:09 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/Jagan/dicee_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Jagan.dicee_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Dicee',
+              style: TextStyle(
+                  fontSize: 27, letterSpacing: 4, fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1; //1 to 6
+      rightDiceNumber = Random().nextInt(6) + 1;
+      print('diceNumber=$leftDiceNumber');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
